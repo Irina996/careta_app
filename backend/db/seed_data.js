6b1a01f69f92bcfca87c3a6ae3216c97185d1477
trigrutson <irishka.klimovich.2002@mail.ru>
2022-12-10 23:19:39 +0300

    add car seed

--- backend/db/seed_data.js
+++ backend/db/seed_data.js
@@ -2,6 +2,8 @@ import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 import pg from 'pg';
 import fs from 'fs';
+import { faker } from '@faker-js/faker';
+import RandExp from 'randexp';
 
 dotenv.config();
 
@@ -50,11 +52,13 @@ img_paths.sort(function (a, b) {
 });
 
 console.log('Adding car groups...');
+let car_groups_length = 0;
 var data_array = fs
     .readFileSync('./db/seed_data/group.txt')
     .toString()
     .split('\n');
 for (var i = 0; i < data_array.length; i++) {
+    car_groups_length = car_groups_length + 1;
     var img;
     await cloudinary.uploader
         .upload('./db/seed_data/images/' + img_paths[i])
@@ -87,5 +91,29 @@ for (var i = 0; i < data_array.length; i++) {
         ]
     );
 }
+
+console.log('Adding colors...');
+let color_length = 0;
+for (var i = 0; i < 5; i++) {
+    color_length = color_length + 1;
+    await pool.query(`INSERT INTO Color(color_name) VALUES ($1);`, [
+        faker.vehicle.color(),
+    ]);
+}
+
+console.log('Adding cars');
+const vid = new RandExp(/\d\d\d\d [A-Z][A-Z]-[1-7]/);
+for (var i = 0; i < 35; i++) {
+    await pool.query(
+        `INSERT INTO Car(car_group_id, color_id, car_number) 
+         VALUES ($1, $2, $3);`,
+        [
+            Math.floor(Math.random() * car_groups_length) + 1,
+            Math.floor(Math.random() * color_length) + 1,
+            vid.gen(),
+        ]
+    );
+}
+
 pool.end();
 console.log('Successfully ended');
