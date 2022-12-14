9902ef6dd0852249fefbdaebaf62a2318c8c2105
iceandrise <iceandrise@gmail.com>
2022-12-14 21:52:24 +0300

    added add/edit/delete admin cars

---
+++ frontend/src/http/carAPI.ts
@@ -0,0 +1,135 @@
+import { $authHost, $host } from "./index";
+
+export interface ICar {
+  group_id: number;
+  brand_name: string;
+  model_name: string;
+  image: string;
+  car_cost: number;
+}
+
+interface IResponse<T> {
+  data: T;
+  message: "successful";
+  success: true;
+}
+
+export enum EUrlKeys {
+  BRAND = "brand",
+  MODEL = "model",
+  START_YEAR = "year_start",
+  END_YEAR = "year_finish",
+  SEATS = "seats_number",
+  END_DATE = "end_date",
+  START_DATE = "start_date",
+  GEARBOX = "gearbox_type",
+  CLASS = "class_name",
+}
+
+export enum ECarFields {
+  BRAND = "brand",
+  MODEL = "model",
+  CLASS = "car_class",
+  YEAR = "creation_year",
+  GEARBOX = "gearbox",
+  SEATS = "seats_number",
+  CONSUMPTION = "fuel_consumption",
+  NUMBER = "car_number",
+  COLOR = "color",
+  COST = "cost",
+  IMAGE = "image",
+}
+
+export interface IAdminFullCar {
+  brand: string;
+  car_class: string;
+  car_id: number;
+  car_number: string;
+  color: string;
+  cost: number;
+  creation_year: number;
+  fuel_consumption: number;
+  gearbox: string;
+  image: string;
+  model: string;
+  seats_number: number;
+}
+
+export type ICreateCar = Record<
+  Exclude<ECarFields, "image">,
+  string | number
+> & {
+  [ECarFields.IMAGE]: File | null;
+};
+export type ICarQueryParams = Partial<Record<EUrlKeys, string>>;
+
+export const createCar = async (car: ICreateCar, token?: string) => {
+  const input = new FormData();
+  for (var key in car) {
+    // @ts-ignore
+    input.append(key, car[key]);
+  }
+  const { data } = await $host.post("/admin/car/add", input, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+export const fetchAdminCar = async (token?: string) => {
+  const { data } = await $host.get<IResponse<Array<ICar>>>("admin/car/", {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+export const deleteCar = async (input: { id: number }, token?: string) => {
+  const { data } = await $host.post("/admin/car/delete/", input, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+export const editCar = async (car: ICreateCar & { id: number }, token?: string) => {
+  const input = new FormData();
+  for (var key in car) {
+    // @ts-ignore
+    input.append(key, car[key]);
+  }
+  const { data } = await $host.post("/admin/car/edit", input, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+}
+
+export const fetchCar = async (props?: { params: ICarQueryParams }) => {
+  const { data } = await $host.get<IResponse<Array<ICar>>>("home", {
+    params: props?.params,
+  });
+  return data;
+};
+
+export const fetchOneCar = async (id: string) => {
+  const { data } = await $host.get("home/car?id=" + id);
+  return data;
+};
+
+export const fetchAdminOneCar = async (id: number, token?: string) => {
+  const { data } = await $host.get<IResponse<IAdminFullCar>>(
+    `admin/car/info/${id}`,
+    {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return data;
+};
--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import { useContext, useEffect } from 'react';
-import { observer } from 'mobx-react-lite';
-import { Container, Col, Form, Button, Row } from 'react-bootstrap';
-import BrandBar from '../components/BrandBar';
-import CarListAdmin from '../components/CarListAdmin';
-import AdminBar from '../components/AdminBar';
-import { fetchCar, fetchType } from '../http/carAPI';
-import { Context } from '../index';
+import React from "react";
+import { useContext, useEffect } from "react";
+import { observer } from "mobx-react-lite";
+import { Container, Col, Form, Button, Row } from "react-bootstrap";
+import BrandBar from "../components/BrandBar";
+import CarListAdmin from "../components/CarListAdmin";
+import { AdminBar } from "../components/AdminBar";
+import { fetchAdminCar, fetchCar, fetchType } from "../http/carAPI";
+import { Context } from "../index";
+import { useAuthContext } from "../contexts";
 
 const Admin = observer(() => {
-    const {car} = useContext(Context)
+  const { car } = useContext(Context);
+  const { token } = useAuthContext();
 
-    useEffect(() => {
-        fetchCar().then(data => car.setCars(data.data))
-    }, [])
+  useEffect(() => {
+    if (token) {
+      fetchAdminCar(token).then((data) => car.setCars(data.data));
+    }
+  }, [token]);
 
-    return (
-        <Container>
-            <Row >
-                <Col md={4}>
-                <div className="d-flex align-items-center">
-                    <h2 className=" mx-auto mt-5 mb-3">Add car</h2>
-                </div>
-                    <AdminBar/>
-                </Col>
-                <Col md={8}>
-                <div className="d-flex align-items-center">
-                    <h2 className=" mx-auto mt-5 mb-5">Available cars</h2>
-                </div>
-                <CarListAdmin/>
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <div className="d-flex align-items-center">
+            <h2 className=" mx-auto mt-5 mb-3">Add car</h2>
+          </div>
+          <AdminBar />
+        </Col>
+        <Col md={8}>
+          <div className="d-flex align-items-center">
+            <h2 className=" mx-auto mt-5 mb-5">Available cars</h2>
+          </div>
+          <CarListAdmin />
+        </Col>
+      </Row>
+    </Container>
+  );
 });
 
 export default Admin;
